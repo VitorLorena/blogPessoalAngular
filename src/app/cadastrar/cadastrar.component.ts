e4060a8c686e8fda719e13a4673d0c892b7f8536
VitorDani <vitord2002@hotmail.com>
2022-01-23 19:53:59 -0300

    consumos de API

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,15 +1,47 @@
+import { AuthService } from '../service/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { User } from '../model/User';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastrar',
-  templateUrl: './cadastrar.component.html',
+  templateUrl: '../cadastrar/cadastrar.component.html',
   styleUrls: ['./cadastrar.component.css']
 })
 export class CadastrarComponent implements OnInit {
 
-  constructor() { }
+  user: User = new User()
+  conferirsenha: string
+  tipoUsuario: string
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) { }
+
+  ngOnInit() {
+  }
+
+  conferSenha(event: any) {
+    this.conferirsenha = event.target.value
+  }
+
+  tipoUser(event: any){
+    this.tipoUsuario = event.target.value
+  }
+
+  cadastrar() {
+    this.user.tipo = this.tipoUsuario
+    
+  if (this.user.senha != this.conferirsenha){
+      alert('Senhas não coincidem')
+    } else {
+      this.authService.cadastrar(this.user).subscribe((resp: User) => {
+        this.user = resp
+        alert('Usuario cadastro com sucesso... ♥')
+        this.router.navigate(['/login'])
+      })
+    }
   }
 
 }
